6fd205cde7eedb795fe36179a09479739b585104
xiezhiwei666 <1825167968@qq.com>
2019-10-22 00:00:48 +0800

    work7

--- work7/index.js
+++ work7/index.js
@@ -1,6 +1,6 @@
 function sort(method){
     //读取待排序数据
-    let origin = document.getElementById('origin').Value
+    let origin = document.getElementById('origin').value
     let logs
     console.log(method)
     //根据传入排序的方法调用不同的排序函数
@@ -8,8 +8,8 @@ function sort(method){
         //调用bubbleSort()冒泡排序函数获取排序结果，
         logs = bubbleSort(origin)
     }else if(method=='insert'){
-        //调用insertSort()插入排序函数获取排序结果，
-        logs = inserSort(origin)
+        //调用insertSort()
+        logs = insertSort(origin)
     }
     //获取显示冒泡排序的div
     showDiv = document.getElementById('Anim')
