e155b731b52005a7fe414e26cc4b638124c2493a
xiezhiwei666 <1825167968@qq.com>
2019-11-20 22:44:45 +0800

    work10

--- work10/Stringstatistics.js
+++ work10/Stringstatistics.js
@@ -1,10 +1,11 @@
 
-function  stat() {
+function stat() {
     let str = document.getElementById("str").value
-    let obj = {}
-    str.split('').reduce((pre, item)  =>  {
-        pre[item] ? pre[item]++ : pre[item] = 1
-        return obj
-    },  {})
+    let arr= str.split('').sort()
+    obj = arr.reduce((pre, item) => {
+        pre[item]?pre[item]++ : pre[item] = 1
+        return pre
+    }, {})
     document.getElementById('result').innerText = JSON.stringify(obj)
 }
+
